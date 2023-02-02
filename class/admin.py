cb4ea539fea6b43a2e1f436f92852708c953ebf8
Mustafo <muhammadjonmominov77@gmail.com>
2023-02-02 21:48:56 +0500

    remove class app

--- class/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from .models import Book
-from import_export.admin import ImportExportActionModelAdmin
-
-@admin.register(Book)
-class BookAdmin(ImportExportActionModelAdmin):
-    search_fields = ('title', 'author')
-    list_display = ('title', 'author', 'count')
-    list_filter = ('author', )
